7c3cb50d14fa64cfeb523100e38787ca8e170ff2
Tessa <0909922@hr.nl>
2020-10-27 13:38:40 +0000

    added filter function and changed the way time is displayed

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,21 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class SearchController extends Controller
-{
+class SearchController extends Controller {
 
-    /*
-     *  'user_id',
-        'image_link',
-        'image_title',
-        'image_description',
-        'image_category',
-     */
+/*https://laravel.com/docs/8.x/eloquent-relationships#constraining-eager-loads*/
 
     function search(Request $request){
         $q = $request->get('q');
-        $images = Image::where('image_title','LIKE','%'.$q.'%')->get();
+        $s = $request->get('s');
+        $images_name = Image::with('user')
+            ->where('image_title','LIKE','%'.$q.'%')
+            ->whereIn('image_category', $s)
+            ->get();
+
+        $images_user = DB::table('images')
+            ->join('users', 'images.user_id', '=', 'users.id')
+            ->where('users.name', 'LIKE', '%'.$q.'%')
+            ->whereIn('images.image_category', $s)
+            ->select('images.*', 'users.name')
+            ->get();
+
+        $images = $images_user;
+
+        foreach ($images_name as $name) {
+            $found_id = false;
+
+            foreach ($images_user as $user) {
+                if($name->id == $user->id) {
+                    $found_id = true;
+                }
+            }
+            if(!$found_id) {
+                $images[] = $name;
+            }
+        }
 
         return response()->json(['images'=>$images]);
     }
--- resources/js/components/SearchComponent.vue
+++ resources/js/components/SearchComponent.vue
@@ -3,22 +3,11 @@
 <!--https://www.digitalocean.com/community/tutorials/vuejs-rest-api-axios-->
 <!--https://jasonwatmore.com/post/2020/07/23/vue-axios-http-get-request-examples-->
 
+
+<!--CSRF token wordt aangeroepen in view_uploads.blade.php en toegewezen aan webcall via functie in bootstrap.js-->
 <template>
     <div class="content">
         <div class="searchbar">
-            <!-- <form action="/search" method="POST" role="search">
-                 <div class="input-group">
-                     <input type="text" class="form-control" name="q"  width="60%" placeholder="Search Image">
-                     <span class="input-group-btn">
-                         <button type="button" class="btn btn-default btn-sm">
-                             <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                 <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                 <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                             </svg>
-                         </button>
-                     </span>
-                 </div>
-             </form>-->
             <b-input-group>
                 <b-form-input v-model="query_data" id="searchQuery" placeholder="Search image">
                 </b-form-input>
@@ -36,6 +25,15 @@
             </b-input-group>
         </div>
 
+        <b-form-group label="Filter:">
+            <b-form-checkbox-group
+                id="checkbox-group-1"
+                v-model="selected"
+                :options="options"
+                name="flavour-1"
+            ></b-form-checkbox-group>
+        </b-form-group>
+
         <div class="table-responsive">
             <table class="table table-hover">
                 <thead>
@@ -44,14 +42,16 @@
                         <th class="cs-p-1">Image</th>
                         <th class="cs-p-1">User</th>
                         <th class="cs-p-1">Date</th>
+                        <th class="cs-p-1">Category</th>
                     </tr>
                 </thead>
                 <tr v-for="image in this.search_result">
                     <td class="cs-p-1">{{ image.image_title }}</td>
 <!--                    https://flaviocopes.com/how-to-remove-last-char-string-js/-->
                     <td class="cs-p-1"><a :href="imageLink.slice(0, -1) + image.id">View Image</a></td>
-                    <td class="cs-p-1">{{ image.image_category }}</td>
-                    <td class="cs-p1-">{{image.created_at}}</td>
+                    <td class="cs-p-1">{{ image.name ?image.name:image.user.name }}</td>
+                    <td class="cs-p1-">{{image.created_at|changeDate}}</td>
+                    <td class="cs-p1-">{{image.image_category}}</td>
                 </tr>
             </table>
         </div>
@@ -64,7 +64,16 @@
         data() {
             return {
                 query_data: '',
-                search_result: []
+                search_result: [],
+                options: [
+                    { text: 'Raid', value: 'raid' },
+                    { text: 'Achievement', value: 'achievement' },
+                    { text: 'Transmogrification', value: 'transmog' },
+                    { text: 'Guild Event', value: 'event' },
+                    {text: 'Other', value: 'other'}
+                ],
+
+                selected: ['raid', 'achievement', 'transmog', 'event', 'other']
             }
         },
 
@@ -80,8 +89,15 @@
 
         methods: {
             search(){
-                axios.get(this.searchRoute, {params:{'q':this.query_data}} )
+                axios.get(this.searchRoute, {params:{'q':this.query_data, 's':this.selected}} )
                     .then(result=>{this.search_result=result.data.images})
+            }
+        },
+
+        filters: {
+            changeDate(date_string){
+                let new_date_string = new Date(date_string);
+                return new_date_string.toLocaleDateString('en-GB');
             }
         }
     }
